5efa36c197b9f3ff340af727252ae1752350bc99
Oskar Nordgren <oskarnordgren@Oskars-MacBook-Air.local>
2014-07-29 15:22:27 +0800

    First commit

---
+++ oni_aliases.zsh
@@ -0,0 +1,38 @@
+#[Aliases]
+alias drri='drush search-reindex -v --immediate'
+alias dr=drush
+alias endev='drush -y en devel_themer'
+alias disdev='drush -y dis devel_themer'
+alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
+alias md='/Applications/ViewDown.app/Contents/MacOS/MarkdownLive'
+#alias drush='nocorrect drush'
+alias ip='ipconfig getifaddr en0'
+alias ios='open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app'
+alias restart='sudo apachectl restart'
+alias amp='sudo apachectl start && mysql.server start'
+
+# edit common files
+alias profile='vim ~/.zshrc'
+
+alias bower='noglob bower'
+
+# Get week number
+alias week='date +%V'
+
+# Hobby
+alias xp='cd ~/Sites/expense && php artisan serve'
+alias webroot='cd /Library/WebServer/Documents'
+
+# Shortcuts
+alias d="cd ~/Documents/Dropbox"
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+alias p="cd ~/Projects"
+
+# Ack
+alias ajs='ack --type=js'
+alias acss='ack --type=scss'
+alias ahtml='ack --type=html'
+
+#alias lines=`wc -l $f | cut -f1 -d' '`
+#alias words=`wc -w $f | cut -f1 -d' ‘`
---
+++ oni_functions.zsh
@@ -0,0 +1,42 @@
+# Determine size of a file or total size of a directory
+function fs() {
+	if du -b /dev/null > /dev/null 2>&1; then
+		local arg=-sbh;
+	else
+		local arg=-sh;
+	fi
+	if [[ -n "$@" ]]; then
+		du $arg -- "$@";
+	else
+		du $arg .[^.]* *;
+	fi;
+}
+
+# Create a new directory and enter it
+function mkd() {
+        mkdir -p "$@" && cd "$@";
+}
+
+# Use Git's colored diff when available
+hash git &>/dev/null;
+if [ $? -eq 0 ]; then
+	function diff() {
+		git diff --no-index --color-words "$@";
+	}
+fi;
+
+# Create a data URL from a file
+function dataurl() {
+	local mimeType=$(file -b --mime-type "$1");
+	if [[ $mimeType == text/* ]]; then
+		mimeType="${mimeType};charset=utf-8";
+	fi
+	echo "data:${mimeType};base64,$(openssl base64 -in "$1" | tr -d '\n')";
+}
+
+# Functions
+#
+# (f)ind by (n)ame
+# usage: fn foo 
+# to find all files containing 'foo' in the name
+function fn() { ls **/*$1*c }
