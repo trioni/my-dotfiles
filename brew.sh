93dba5105e8ee47b47b5c3f2b1e08e4d2a67abe3
Oskar Nordgren <oskar.nordgren@gmail.com>
2016-02-04 14:34:08 +0800

    Updates

---
+++ brew.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Install more recent versions of some OS X tools.
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+brew install homebrew/php/php56 --with-gmp
+
+# Easily search file contents
+brew install ack
+
+# For quickly just between folders
+brew install fasd
+
+brew install tree
+brew install tig
