8992c8aa5020f0137f0efe5d2432f3e4e92a8acb
Oskar Nordgren <oskar.nordgren@tink.com>
2023-06-16 15:48:07 +0200

    include key repeat settings

--- _setup.sh
+++ _setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# Install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 # Move files into place
-cp .gitconfig .vimrc ~/
+ls -s .vimrc ~/.vimrc
+ls -s .tigrc ~/.tigrc
+ls -s .ackrc ~/.ackrc
 
 # Increase the key repeat speed
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+
+# key repeat for VSCode
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+# To enable global key-repeat
+# this is helpful if you're using Vim in a PWA like code-server
+defaults write -g ApplePressAndHoldEnabled -bool false
