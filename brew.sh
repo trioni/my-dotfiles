46b1045100778fb911c45bf3809d788ecada728a
Oskar Nordgren <oskar.nordgren@tink.com>
2023-06-21 21:48:43 +0200

    minor updates to brew script

--- brew.sh
+++ brew.sh
@@ -14,32 +14,23 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade --all
 
-# Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-brew install homebrew/php/php56 --with-gmp
-
 # Easily search file contents
 brew install ack
 
 # For quickly just between folders
 brew install fasd
-
 brew install tree
 brew install tig
 brew install jq
 
 # Quick look plugins
-brew cask install qlcolorcode
-brew cask install qlstephen
-brew cask install qlmarkdown
-brew cask install quicklook-json
-brew cask install betterzip
-brew cask install qlimagesize
-brew cask install webpquicklook
-brew cask install imageoptim
+brew install --cask qlcolorcode
+# Plain text files without extension
+brew install --cask qlstephen
+brew install --cask qlmarkdown
+brew install --cask quicklook-json
+brew install --cask betterzip
+brew install --cask imageoptim
 
 brew tap homebrew/cask-fonts
 brew cask install font-fira-code
